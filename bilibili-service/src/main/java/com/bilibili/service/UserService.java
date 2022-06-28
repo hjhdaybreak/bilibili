005872ba46c0f09df4d2af1dd22f47ff1441d5f9
hjhdaybreak <1923508176@qq.com>
2022-06-29 01:08:58 +0800

    双token 延长登录时间

--- bilibili-service/src/main/java/com/bilibili/service/UserService.java
+++ bilibili-service/src/main/java/com/bilibili/service/UserService.java
@@ -3,6 +3,7 @@ package com.bilibili.service;
 import com.alibaba.fastjson.JSONObject;
 import com.bilibili.dao.UserDao;
 import com.bilibili.domain.PageResult;
+import com.bilibili.domain.RefreshTokenDetail;
 import com.bilibili.domain.User;
 import com.bilibili.domain.UserInfo;
 import com.bilibili.domain.constant.UserConstant;
@@ -15,10 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class UserService {
@@ -138,5 +136,54 @@ public class UserService {
             list = userDao.pageListUserInfos(params);
         }
         return new PageResult<>(total, list);
+    }
+
+    public Map<String, Object> loginForDts(User user) throws Exception {
+        String phone = user.getPhone();
+        if (StringUtils.isNullOrEmpty(phone)) {
+            throw new ConditionException("phone number can not be null !");
+        }
+        User dbUser = this.getUserByPhone(phone);
+        if (dbUser == null) {
+            throw new ConditionException("user is not exist !");
+        }
+        String password = user.getPassword();
+        String rawPassword;
+        try {
+            rawPassword = RSAUtil.decrypt(password);
+        } catch (Exception e) {
+            throw new ConditionException("decrypt fail !");
+        }
+        String salt = dbUser.getSalt();
+        String md5PassWod = MD5Util.sign(rawPassword, salt, StandardCharsets.UTF_8.name());
+        if (!md5PassWod.equals(dbUser.getPassword())) {
+            throw new ConditionException("secret is not correct !");
+        }
+
+        Long userId = dbUser.getId();
+        String accessToken = TokenUtil.generateToken(userId);
+        String refreshToken = TokenUtil.generateRefreshToken(userId);
+
+        //保存 refresh token 到数据库
+        userDao.deleteRefreshToken(refreshToken, userId);
+        userDao.addRefreshToken(refreshToken, userId, new Date());
+        Map<String, Object> result = new HashMap<>();
+        result.put("accessToken", accessToken);
+        result.put("refreshToken", refreshToken);
+        return result;
+
+    }
+
+    public void logout(String refreshToken, Long userId) {
+        userDao.deleteRefreshToken(refreshToken, userId);
+    }
+
+    public String refreshAccessToken(String refreshToken) throws Exception {
+        RefreshTokenDetail refreshTokenDetail = userDao.getRefreshTokenDetail(refreshToken);
+        if (refreshTokenDetail.getRefreshToken() == null) {
+            throw new ConditionException("555", "token过期 !");
+        }
+        Long userId = refreshTokenDetail.getUserId();
+        return TokenUtil.generateToken(userId);
     }
 }
--- bilibili-service/src/main/java/com/bilibili/service/util/TokenUtil.java
+++ bilibili-service/src/main/java/com/bilibili/service/util/TokenUtil.java
@@ -18,7 +18,7 @@ public class TokenUtil {
         Algorithm algorithm = Algorithm.RSA256(RSAUtil.getPublicKey(), RSAUtil.getPrivateKey());
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
-        calendar.add(Calendar.SECOND, 300000000);
+        calendar.add(Calendar.HOUR, 1);
         return JWT.create()
                 .withKeyId(String.valueOf(userId))
                 .withIssuer(ISSUER)
@@ -38,5 +38,17 @@ public class TokenUtil {
         } catch (Exception e) {
             throw new ConditionException("非法token !");
         }
+    }
+
+    public static String generateRefreshToken(Long userId) throws Exception {
+        Algorithm algorithm = Algorithm.RSA256(RSAUtil.getPublicKey(), RSAUtil.getPrivateKey());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DAY_OF_MONTH, 7);
+        return JWT.create()
+                .withKeyId(String.valueOf(userId))
+                .withIssuer(ISSUER)
+                .withExpiresAt(calendar.getTime())
+                .sign(algorithm);
     }
 }
