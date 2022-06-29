76f07d2a0ab5843b8ef7d82285df917b196723b6
hjhdaybreak <1923508176@qq.com>
2022-06-29 21:16:40 +0800

    断点续传

---
+++ bilibili-api/src/main/java/com/bilibili/api/DemoApi.java
@@ -0,0 +1,22 @@
+package com.bilibili.api;
+
+import com.bilibili.service.config.FastDFSUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+public class DemoApi {
+
+    @Autowired
+    private FastDFSUtil fastDFSUtil;
+
+    @GetMapping("/slices")
+    public void slices(MultipartFile file) throws Exception {
+        fastDFSUtil.convertFileToSlices(file);
+    }
+
+}
---
+++ bilibili-api/src/main/java/com/bilibili/api/FileApi.java
@@ -0,0 +1,27 @@
+package com.bilibili.api;
+
+import com.bilibili.domain.JsonResponse;
+import com.bilibili.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileApi {
+
+    @Autowired
+    private FileService fileService;
+
+
+    @PutMapping("/file-slices")
+    public JsonResponse<String> uploadFileBySlices(MultipartFile slice,
+                                                   String fileMd5,
+                                                   Integer sliceNo,
+                                                   Integer totalSliceNo) throws Exception {
+        String filePath = fileService.uploadFileBySlices(slice, fileMd5, sliceNo, totalSliceNo);
+        return new JsonResponse<>(filePath);
+    }
+
+}
